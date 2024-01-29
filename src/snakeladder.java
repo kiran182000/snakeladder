60fe128850ff4e556cb4d0235c77d733e693340b
kiran <kiran405411@gmail.com>
2024-01-29 14:11:38 +0530

    checking for position

--- src/snakeladder.java
+++ src/snakeladder.java
@@ -1,6 +1,24 @@
+import java.util.*;
 public class snakeladder {
     public static void main(String[] args){
         int RollDice=(int)(Math.random()*6+1);
         System.out.println("Dice number after rolling: "+RollDice);
+        int Option=(int)(Math.random()*3+1);
+        int position=0;
+        switch(Option)
+        {
+            case 1:
+                System.out.println("NO PLAY: "+position);
+                System.out.println("Player stays in the same position");
+                break;
+            case 2:
+                System.out.println("LADDER: "+(RollDice+position));
+                System.out.println("Player moves ahead by: "+RollDice);
+                break;
+            case 3:
+                System.out.println("SNAKE: "+(RollDice+position));
+                System.out.println("Player moves behind by: "+RollDice);
+
+        }
     }
 }
